b3d6259c41493904b9572a5ce9cc4f4f344ca863
xueshanpeng <289159468@qq.com>
2015-12-08 16:55:28 +0800

    server

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,21 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
-AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+// AV.Cloud.define("hello", function(request, response) {
+//   response.success("Hello world!");
+// });
+
+var express = require('express');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(function(req, res){
+	res.type('text/plain');
+	res.status(404);
+	res.end('404 not found');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('app started');
 });
