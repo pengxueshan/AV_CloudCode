7a981515aa91e3527ce73419c09152d5c031a70c
xueshanpeng <289159468@qq.com>
2015-12-08 17:31:14 +0800

    server test

--- cloud/main.js
+++ cloud/main.js
@@ -5,10 +5,21 @@
 // });
 
 var express = require('express');
+var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
 
 var app = express();
+app.engine('handlebars', handlebars.engine);
+app.set('views', '/views');
+app.set('view engine', 'handlebars');
+
 
 app.set('port', process.env.PORT || 3000);
+
+// app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+	res.render('home');
+});
 
 app.use(function(req, res){
 	res.type('text/plain');
